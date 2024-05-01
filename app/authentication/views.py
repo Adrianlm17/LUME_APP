b325abecf1448e4fb2dc63ca63e05c669f764e88
Adrianlm17 <adrianlomu2004@gmail.com>
2024-05-01 20:32:36 +0200

    feat(app): implementar base de la aplicación.

--- app/authentication/views.py
+++ app/authentication/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from .forms import LoginForm, SignUpForm
 
 
@@ -48,3 +48,8 @@ def register_user(request):
         form = SignUpForm()
 
     return render(request, "accounts/register.html", {"form": form, "msg": msg, "success": success})
+
+def logout_view(request):
+    
+    logout(request)
+    return redirect('/login/login/')
